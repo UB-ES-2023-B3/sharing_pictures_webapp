bf1d2365b796f067ee1dd27d75be9f7eeb2789b1
Pablo María Arranz Pou <pablomariaarranzpou@gmail.com>
2023-10-12 15:32:36 +0200

    BACK I FRONT COONECTAT toca testing

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('load_pictures/', views.load_pictures, name='load_more_pictures'),
-    path('upload/', views.upload_picture, name='upload_picture')
-]
+    path('upload/', views.upload_picture, name='upload_picture'),
+
+
+    ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,21 @@
 from .models import Post
 from .forms import UploadPostForm
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 def load_pictures(request):
     posts = Post.objects.all()
     picture_data = []
 
     for post in posts:
-        for i in range(12):
-            picture_data.append({
+        picture_data.append({
                 'image_url': post.image.url,
                 'description': post.description,
                 'created_at': post.created_at.strftime('%F %d, %Y'),
                 'image_size': post.image.size,
-            })
+        })
 
     return JsonResponse({'pictures': picture_data}, safe=False)
 
@@ -24,7 +25,10 @@ def upload_picture(request):
         form = UploadPostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('load_more_pictures')
+            return load_pictures(request) 
+        else:
+            return HttpResponseBadRequest("Form validation failed")
     else:
-        form = UploadPostForm()
-    return render(request, 'main_app/post_upload_template.html', {'form': form})
+        load_pictures(request) 
+
+
