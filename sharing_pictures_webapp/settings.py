b605cbd4666e42b0b84f499198e32d643d28382d
quimlr <quimlagunas@gmail.com>
2023-10-13 21:38:52 +0200

    Implemented basic register (that still lacks some US restrictions)

--- sharing_pictures_webapp/settings.py
+++ sharing_pictures_webapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig' #US1.1, US2.1
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_URL = 'login' #US2.1
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.sign_up, name='register'), #US1.1
+    path('login/', views.sign_in, name='login'), #US2.1
+    path('logout/', views.sign_out, name='logout'), #USX.X (logout)
+]
