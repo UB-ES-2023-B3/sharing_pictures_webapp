e911037cfe6d2a21a66863325e90c5c6d2ee9c64
Jan <janmanelmorales@gmail.com>
2023-11-18 12:34:19 +0100

    Test models and forms

--- backend/main_app/tests/test_models/test_custom_user.py
+++ backend/main_app/tests/test_models/test_custom_user.py
@@ -1,34 +1,36 @@
 # backend/main_app/tests/test_models/test_custom_user.py
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from backend.main_app.models import CustomUser
 
 class CustomUserModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        # Set up non-modified objects used by all test methods
-        CustomUser.objects.create_user(email='testuser@example.com', username='testuser', password='12345')
+        cls.user = CustomUser.objects.create_user(email='testuser@example.com', username='testuser', password='12345')
 
     def test_email_label(self):
-        user = CustomUser.objects.get(id=1)
-        field_label = user._meta.get_field('email').verbose_name
-        self.assertEquals(field_label, 'email')
+        field_label = self.user._meta.get_field('email').verbose_name
+        self.assertEqual(field_label, 'email')
 
     def test_email_unique(self):
-        user = CustomUser.objects.get(id=1)
-        unique = user._meta.get_field('email').unique
+        unique = self.user._meta.get_field('email').unique
         self.assertTrue(unique)
 
     def test_username_label(self):
-        user = CustomUser.objects.get(id=1)
-        field_label = user._meta.get_field('username').verbose_name
-        self.assertEquals(field_label, 'username')
+        field_label = self.user._meta.get_field('username').verbose_name
+        self.assertEqual(field_label, 'username')
 
     def test_object_name_is_username(self):
-        user = CustomUser.objects.get(id=1)
-        expected_object_name = user.username
-        self.assertEquals(expected_object_name, str(user))
+        self.assertEqual(self.user.username, str(self.user))
 
     def test_required_fields(self):
-        required_fields = CustomUser.REQUIRED_FIELDS
-        self.assertTrue('username' in required_fields)
-        self.assertFalse('email' in required_fields)
+        self.assertIn('username', CustomUser.REQUIRED_FIELDS)
+        self.assertNotIn('email', CustomUser.REQUIRED_FIELDS)
+
+    def test_user_creation_with_valid_email(self):
+        self.assertEqual(self.user.email, 'testuser@example.com')
+
+    def test_user_creation_with_invalid_email(self):
+        with self.assertRaises(ValidationError):
+            invalid_user = CustomUser.objects.create_user(email='not-an-email', username='testuser2', password='12345')
+            invalid_user.full_clean()  # This will check for validation errors.
